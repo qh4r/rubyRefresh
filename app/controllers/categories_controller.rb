2fe47f243bac9ed09b661c8c5eeec5de2de33c4d
Rafał Kucharski <r.kucharski@lgbs.pl>
2016-08-17 21:53:54 +0200

    Added create category form and categories display with tests

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,15 +1,30 @@
 class CategoriesController < ApplicationController
 
   def index
-
+    @categories = Category.all
   end
 
   def new
+    @category = Category.new
+  end
 
+  def create
+    @category = Category.new(parse_category_params)
+    if @category.save
+      flash[:success] = 'Utworzono z powodzeniem'
+      redirect_to categories_path
+    else
+      render new_category_path
+    end
   end
 
   def show
 
+  end
+
+  private
+  def parse_category_params
+    params.require(:category).permit(:name)
   end
 
 end
---
+++ test/integration/create_categories_test.rb
@@ -0,0 +1,31 @@
+require 'test_helper'
+
+class CreateCategoriesTest < ActionDispatch::IntegrationTest
+
+  test 'get new category form and create category' do
+    get new_category_path
+    assert_template 'categories/new'
+    assert_difference 'Category.count', 1 do
+      post_via_redirect categories_path, category: {name: 'ksiazki'}
+    end
+    assert_template 'categories/index'
+    assert_match 'ksiazki', response.body
+  end
+
+  test 'wrong category name fails to create new category' do
+    get new_category_path
+    assert_template 'categories/new'
+    assert_no_difference 'Category.count' do
+      # tutaj nie ma redirecta?
+      # metoda post po prostu sledzi 1 request,
+      # metoda post_via_redirect sledzi request i wszystkie redirecty
+      post categories_path, category: {name: 'dupa'}
+    end
+    assert_template 'categories/new'
+    assert_match 'error', response.body
+    # wybiera element na stronie i przechodzi jesli sie udalo
+    # standardowa skladnia
+    assert_select 'div.panel.panel-danger'
+  end
+
+end
