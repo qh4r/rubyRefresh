5349f305911944fa6d1be7cd4ac63c0841da7e5f
Rafał Kucharski <r.kucharski@lgbs.pl>
2016-10-05 20:41:13 +0200

    Generated bootstrap and devise related stuff

--- gallery-app/app/controllers/welcome_controller.rb
+++ gallery-app/app/controllers/welcome_controller.rb
@@ -1,4 +1,16 @@
+# rails generate controller welcome index
+# generuje takze route i widok
+
+#potem generacja w user.rb i   migracje
+
+# rails generate bootstrap:install static
+# rails generate bootstrap:layout application
+# rails generate devise:views:locale pl
+# rails g devise:views:bootstrap_templates
+
 class WelcomeController < ApplicationController
+  # skip tego co ejst zdefiniowane w ApplicationControllerze
+  skip_before_action :authenticate_user!, only: [:index]
   def index
   end
 end
---
+++ gallery-app/app/models/user.rb
@@ -0,0 +1,9 @@
+# rails generate devise:install
+# rails generate devise User
+
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :confirmable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
